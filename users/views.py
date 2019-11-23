1a49db4d71608a54be57951f22092e6597edf538
Brian Ogutu <codingbrian58@gmail.com>
2019-11-23 15:46:31 +0300

    Add url configurations

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse,request
 
-# Create your views here.
+def index(request):
+    return HttpResponse('Hello there welcome to idea hub')
+    
