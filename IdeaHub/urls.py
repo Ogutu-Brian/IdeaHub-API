005b0a5ade40dfbe6d275dab2d5f1841913e9f70
Brian Ogutu <codingbrian58@gmail.com>
2020-05-21 01:16:47 +0300

    Change admin site title

--- IdeaHub/urls.py
+++ IdeaHub/urls.py
@@ -4,6 +4,9 @@ from django.contrib import admin
 from .utils.routing import get_app_routes
 from IdeaHub.apps import authentication
 
+admin.site.site_header = 'IdeaHub'
+admin.site.index_title = 'IdeaHub Administration'
+
 urlpatterns = [
     path('authentication/', include(
         get_app_routes(authentication.APP_NAME)
