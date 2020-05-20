ece6e30ffd5a08834d63d98f27eb619635bd7ede
Brian Ogutu <codingbrian58@gmail.com>
2020-05-20 16:47:17 +0300

    Remove unnecessary urls

--- IdeaHub/urls.py
+++ IdeaHub/urls.py
@@ -1,27 +1,6 @@
 from django.conf.urls import include
 from django.urls import path
-from django.contrib.auth.models import User
-from django.contrib import admin
-from rest_framework import routers, serializers, viewsets
-
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email']
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-router = routers.DefaultRouter()
-router.register('users', UserViewSet)
-
 
 urlpatterns = [
-    path('admin', admin.site.urls),
-    path('', include(router.urls)),
     path('profile/', include('IdeaHub.apps.profile.urls')),
 ]
