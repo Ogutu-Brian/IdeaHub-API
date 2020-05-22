2f343252f05f240e3318a41d3b36dfd50dbc9b60
Brian Ogutu <codingbrian58@gmail.com>
2020-05-22 11:58:36 +0300

    Handle string rendering on the database tables

--- IdeaHub/apps/profile/models.py
+++ IdeaHub/apps/profile/models.py
@@ -12,6 +12,9 @@ class Profile(models.Model):
     )
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return '{} {}'.format(self.user.first_name, self.user.last_name)
+
 
 class VerificationCode(models.Model):
     id = models.UUIDField(
@@ -22,3 +25,6 @@ class VerificationCode(models.Model):
     )
     code = models.CharField(max_length=250)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.code
